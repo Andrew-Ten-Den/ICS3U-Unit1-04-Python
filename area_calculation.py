1e3aa8ede367c0fe158b49bbaad79b0314d02e4b
Andrew-Ten-Den <andrew.ten-den@mths.ca>
2022-02-24 19:38:26 +0000

    added area calculation

---
+++ area_calculation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by: Andrew-Ten-Den
+# Created on: February 24 2020
+# This program calculates the area and perimeter of a rectangle
+# with dimensions of 5cm x 3cm
+
+
+def main():
+    # This program calculates the area and the perimeter of a rectangle
+    print("If a rectangle has the dimensions of: ")
+    print("5cm by 3cm")
+    print("")
+    print("Area is {}cm².".format(3 * 5))
+    print("Perimeter is {}cm.".format((3 + 5) * 2))
+    print("")
+    print("Problem Solved")
+
+
+if __name__ == "__main__":
+    main()
