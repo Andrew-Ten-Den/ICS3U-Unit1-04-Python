73a3f43d005d929fbff57cfcecc213306e58d2ba
Andrew-Ten-Den <andrew.ten-den@mths.ca>
2022-03-01 13:40:02 +0000

    fixed area calculation

--- area_calculation.py
+++ area_calculation.py
@@ -3,18 +3,18 @@
 # Created by: Andrew-Ten-Den
 # Created on: February 24 2022
 # This program calculates the area and perimeter of a rectangle
-# with dimensions of 5cm x 3cm
+# with dimensions of 5 cm x 3 cm
 
 
 def main():
     # This program calculates the area and the perimeter of a rectangle
     print("If a rectangle has the dimensions of: ")
-    print("5cm by 3cm")
+    print("5 cm by 3 cm")
     print("")
     print("Area is {} cm².".format(3 * 5))
     print("Perimeter is {} cm.".format((3 + 5) * 2))
     print("")
-    print("Problem Solved")
+    print("Done")
 
 
 if __name__ == "__main__":
